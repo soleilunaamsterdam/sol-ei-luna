0581bf8dd5826be54ea930e1584af6ba9c9258aa
soleilunaamsterdam <admin@soleiluna.nl>
2025-10-25 23:45:17 +0200

    try commit

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu(){
   nav.style.display = isOpen ? 'none' : 'flex';
 }
 document.getElementById('year').textContent = new Date().getFullYear();
+
+
+window.addEventListener("scroll", function() {
+  const header = document.querySelector(".site-header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
